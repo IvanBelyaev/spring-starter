bf383dc664de43c5a6e61c7e3ca9dc330ae2ad84
ivan.belyaev <belivan84@gmail.com>
2023-12-21 23:19:38 +0300

    аннотация для интеграционных тестов, внедрение бинов через конструктор

--- src/test/java/org/example/spring/integration/CompanyServiceIT.java
+++ src/test/java/org/example/spring/integration/CompanyServiceIT.java
@@ -1,26 +1,24 @@
 package org.example.spring.integration;
 
+import lombok.RequiredArgsConstructor;
 import org.example.spring.config.DatabaseProperties;
 import org.example.spring.dto.CompanyReadDto;
+import org.example.spring.integration.annotation.IT;
 import org.example.spring.service.CompanyService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public class CompanyServiceIT {
     private static final Long COMPANY_ID = 1L;
 
-    @Autowired
-    private CompanyService companyService;
-    @Autowired
-    private DatabaseProperties databaseProperties;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
