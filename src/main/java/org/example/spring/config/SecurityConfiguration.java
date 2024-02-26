fc4d63dde8a89e15d0c006461f16563f99aba7fa
ivan.belyaev <belivan84@gmail.com>
2024-02-26 22:41:31 +0300

    CSRF Filter

--- src/main/java/org/example/spring/config/SecurityConfiguration.java
+++ src/main/java/org/example/spring/config/SecurityConfiguration.java
@@ -2,10 +2,10 @@ package org.example.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +20,7 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(Customizer.withDefaults())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/login", "/users/registration",
                                 "/v3/api-docs/**", "/swagger-ui/**").permitAll()
