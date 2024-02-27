43be5e4177c6be6e108a7a02d77f16a3152ed8e9
ivan.belyaev <belivan84@gmail.com>
2024-02-28 00:45:53 +0300

    OAuth 2.0

--- src/main/java/org/example/spring/config/SecurityConfiguration.java
+++ src/main/java/org/example/spring/config/SecurityConfiguration.java
@@ -1,21 +1,32 @@
 package org.example.spring.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.spring.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 import static org.example.spring.database.entity.Role.ADMIN;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final UserService userService;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,7 +34,7 @@ public class SecurityConfiguration {
                 .csrf(Customizer.withDefaults())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/login", "/users/registration",
-                                "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                                "/v3/api-docs/**", "/swagger-ui*/**").permitAll()
                         .requestMatchers("/users/{id}/delete").hasAuthority(ADMIN.getAuthority())
                         .requestMatchers("/admin/**").hasAuthority(ADMIN.getAuthority())
                         .anyRequest().authenticated()
@@ -31,19 +42,35 @@ public class SecurityConfiguration {
 //              .httpBasic(Customizer.withDefaults())
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
-                        .permitAll()
                         .defaultSuccessUrl("/users")
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .deleteCookies("JSESSIONID")
                         .logoutSuccessUrl("/login")
+                )
+                .oauth2Login(config -> config
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService()))
                 );
         return http.build();
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return userRequest -> {
+            String email = userRequest.getIdToken().getClaim("email");
+            UserDetails userDetails = userService.loadUserByUsername(email);
+            // TODO если пользователь не найден, то создать пользователя через userService.create()
+            var oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+
+            var userDetailsMethods = Set.of(UserDetails.class.getMethods());
+
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OidcUser.class},
+                    (proxy, method, args) -> userDetailsMethods.contains(method)
+                            ? method.invoke(userDetails, args)
+                            : method.invoke(oidcUser, args));
+        };
     }
 }
